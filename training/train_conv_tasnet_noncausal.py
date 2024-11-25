5b90b2a4cf2f723a2367c18fb74a29247dbb771e
sandro7202004 <129913027+sandro7202004@users.noreply.github.com>
2024-11-25 16:01:33 +0100

    Update train_conv_tasnet_noncausal.py

--- training/train_conv_tasnet_noncausal.py
+++ training/train_conv_tasnet_noncausal.py
@@ -1,21 +1,16 @@
 import sys
 import os
-
-# Add the root directory to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 import torch
 from models.conv_tasnet import build_conv_tasnet  # Conv-TasNet model
 from training.losses.si_snr import SISNRLoss     # SI-SNR loss function
 from data.dataloader import EARSWHAMDataLoader  # Your custom dataloader class
 import logging
 
+# Add the root directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
-
-import logging 
-logger = logging.getLogger(__name__)
-logging.basicConfig(filename='noncausal.log', encoding='utf-8', level=logging.DEBUG)
 
 # Initialize data loaders
 data_loader = EARSWHAMDataLoader(
@@ -81,7 +76,5 @@ for epoch in range(num_epochs):
 
     avg_val_loss = val_loss / len(valid_loader)
 
-
     logger.info(f"Epoch {epoch + 1}/{num_epochs}, Train Loss: {avg_train_loss:.4f}, Validation Loss: {avg_val_loss:.4f}")
     print(f"Epoch {epoch + 1}/{num_epochs}, Train Loss: {avg_train_loss:.4f}, Validation Loss: {avg_val_loss:.4f}")
-
