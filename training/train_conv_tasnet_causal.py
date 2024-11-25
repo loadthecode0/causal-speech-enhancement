b3abc8eb117f2ff92b8f23b4b9df4874aef46e39
loadthecode0 <artyaastha@gmail.com>
2024-11-25 15:47:43 +0100

    minor fixes

--- training/train_conv_tasnet_causal.py
+++ training/train_conv_tasnet_causal.py
@@ -27,7 +27,7 @@ valid_loader = data_loader.get_loader(split="valid")
 
 # Initialize model, loss, and optimizer
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = build_conv_tasnet(causal=False, num_sources=2).to(device)
+model = build_conv_tasnet(causal=True, num_sources=2).to(device)
 criterion = SISNRLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 logger.info('Model, loss and optimizer initialized')
