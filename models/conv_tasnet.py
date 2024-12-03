1f47884335788eba1eb4c143cb7c2c4e5db87336
sandro7202004 <129913027+sandro7202004@users.noreply.github.com>
2024-12-03 19:44:31 +0100

    Update conv_tasnet.py

--- models/conv_tasnet.py
+++ models/conv_tasnet.py
@@ -25,7 +25,7 @@ class CausalConvTranspose1D(nn.Module):
         padding (int, optional): Padding added to the input (defaults to 0).
         output_padding (int, optional): Extra padding added to the output.
     """
-    def __init__(self, in_channels, out_channels, kernel_size, stride, output_padding=0):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, output_padding=0,bias):
         super(CausalConvTranspose1D, self).__init__()
         self.kernel_size = kernel_size
         self.stride = stride
@@ -36,6 +36,7 @@ class CausalConvTranspose1D(nn.Module):
             kernel_size,
             stride=stride,
             output_padding=output_padding,
+            bias=bias,
         )
 
     def forward(self, x):
@@ -44,7 +45,7 @@ class CausalConvTranspose1D(nn.Module):
         return self.conv_transpose(x)
 
 class CausalConv1D(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, dilation=1, groups=1):
+    def __init__(self, in_channels, out_channels, kernel_size, dilation=1, groups=1, bias):
         super(CausalConv1D, self).__init__()
         self.left_padding = (kernel_size - 1) * dilation
         self.conv = nn.Conv1d(
@@ -53,6 +54,7 @@ class CausalConv1D(nn.Module):
             kernel_size,
             dilation=dilation,
             groups=groups,
+            bias=bias,
         )
 
     def forward(self, x):
